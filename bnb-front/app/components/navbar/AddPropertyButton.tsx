2518b7823ce2757698fc93fe0d6513f5906cd134
Muhammad Hannan <mdhannan.info@gmail.com>
2024-05-19 11:09:08 +0600

    Update navbar

---
+++ bnb-front/app/components/navbar/AddPropertyButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddPropertyButton = () => {
+    return (
+        <div
+            className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-200"
+        >
+            Your home
+        </div>
+    )
+}
+
+export default AddPropertyButton
